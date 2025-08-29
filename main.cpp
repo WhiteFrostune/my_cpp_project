f656e464a7e622680cd379331ab2ec3331573f1f
WhiteFrostune <3566621734@qq.com>
2025-08-29 20:36:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
-#include <stdlib.h> // 需要包含这个头文件来使用 system() 命令
 using namespace std;
 int main() {
-    system("chcp 65001"); // 设置控制台为 UTF-8 编码
     cout << "启动！！！" << endl;
     return 0;
 }
+
