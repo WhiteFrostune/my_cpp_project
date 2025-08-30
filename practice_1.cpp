43af7a7f3de4ff77b17e398fbf61fa031328dc2e
WhiteFrostune <3566621734@qq.com>
2025-08-30 15:44:10 +0800

    Add files via upload

---
+++ practice_1.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	cout << "hello world" << endl;
+	cin.get();
+	return 0;
+}
---
+++ practice_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//数据类型 变量名=初始值；
+	int x = 1314;
+	int y = 520;
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl << endl;
+	cout << "Today is Chinese Valentine's Day." << endl << endl;
+	cout << "And I want to tell you something:" << endl;
+	cout << endl << x << " " << y << endl;
+	cin.get();
+	return 0;
+}
