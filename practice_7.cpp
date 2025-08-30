5832aa7448c8c8006113433e89c8f2863abbbc8b
WhiteFrostune <3566621734@qq.com>
2025-08-30 20:27:36 +0800

    Add files via upload

---
+++ practice_7.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	char a = 'y';    // 声明字符变量a
+	char b = 'z';
+
+	cout << a << endl;           
+	cout << int(a) << endl;      // 将a转换为整数输出（ASCII码）
+	cout << b - a << endl;       // 计算b和a的ASCII码差值
+
+	b = 120;                    // 将整数120赋值给字符b
+	cout << b << endl;          
+	cin.get();
+	return 0;
+}
---
+++ practice_8.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+/*
+i=input
+o=output
+*/
+
+using namespace std;
+
+int main() {
+	cout << "请开始您的输入：\n";
+	// 1.整型的输入
+	int a;
+	cin >> a;
+	cout << "a的值为：" << a << endl;
+	// 2.浮点型的输入
+	double b;
+	cin >> b;
+	cout << "b的值为：" << b << endl;
+	// 3.字符型的输入
+	char c;
+	cin >> c;
+	cout << "c的值为：" << c << endl;
+	// 4.字符串型的输入
+	string d;
+	cin >> d;
+	cout << "d的值为：" << d << endl;
+	// 5.布尔类型的输入
+	bool e = false;
+	e = !e; // 取反，即为true
+	cin >> e;
+	cout << "e的值为：" << e << endl;
+	return 0;
+}
