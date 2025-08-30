34962ac8ab0cacc63886d04353f92b5881dc3403
WhiteFrostune <3566621734@qq.com>
2025-08-30 17:14:25 +0800

    Add files via upload

---
+++ practice_4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a12_ = 1314;
+	// int 8a_ = 520; 标识符的第一个字符不能是数字，标识符也不能用关键词做变量或常量
+	int a = 5;
+	int A = 6;
+	cout << a << A << endl;
+	int appleCount = 0;
+	int xiguashuliang = 0;
+	cin.get();
+	return 0;
+}
