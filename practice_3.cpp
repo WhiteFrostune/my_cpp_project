ab59e617db67f7542caa6742c47753982e3901f1
WhiteFrostune <3566621734@qq.com>
2025-08-30 16:47:33 +0800

    Add files via upload

---
+++ practice_3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+#define X 1+2 //宏定义 常量
+int x = 7; //变量
+const int y = 1; //常量
+int main() {
+	cout <<  X * X  << endl;
+	x = (x + 1);
+	cout << x + y << endl;
+	cin.get();
+	return 0;
+}
