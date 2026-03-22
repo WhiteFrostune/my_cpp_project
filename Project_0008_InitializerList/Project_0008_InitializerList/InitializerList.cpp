b10bf3193a131359ca9b13d70cafe9fd51236442
WhiteFrostune <3566621734@qq.com>
2026-03-22 11:36:41 +0800

    Add a Initializerlist exercise

---
+++ Project_0008_InitializerList/Project_0008_InitializerList/InitializerList.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+初始化列表的语法
+
+构造函数（传参1，传参2）：成员变量1（传参1），成员变量2（传参2）{}
+
+*/
+
+class Hero {
+public:
+	/*Hero(string name, int hp) {
+		m_Name = name;
+		m_Hp = hp;
+	}*/
+
+	Hero(string name, int hp) : m_Name("猴子"), m_Hp(50){
+
+	}
+
+	void Print() {
+		cout << "英雄：" << m_Name << "的血量是" << m_Hp << endl;
+	}
+
+private:
+	string m_Name;
+	int m_Hp;
+};
+
+int main() {
+	Hero h("剑圣", 100);
+	h.Print();
+
+	return 0;
+}
