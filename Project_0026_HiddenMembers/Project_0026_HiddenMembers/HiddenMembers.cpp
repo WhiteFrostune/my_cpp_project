d4c7e486c60665b76a455530c5a2bf112a16f168
WhiteFrostune <3566621734@qq.com>
2026-03-31 22:38:07 +0800

    Add inheritance exercises: hidden members, overridden functions, multiple inheritance

---
+++ Project_0026_HiddenMembers/Project_0026_HiddenMembers/HiddenMembers.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	Animal() {
+		m_Data = 17981;
+	}
+	int m_Data;
+};
+
+class Cat : public Animal {
+public:
+	Cat() {
+		m_Data = 27982;
+	}
+	int m_Data;
+};
+
+void Test() {
+	Cat c;
+	cout << c.m_Data << endl;
+	cout << c.Animal::m_Data << endl;
+
+	cout << &(c.m_Data) << endl;
+	cout << &(c.Animal::m_Data) << endl;
+}
+
+int main() {
+	Test();
+	return 0;
+}
