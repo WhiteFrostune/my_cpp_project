005b737678320e764e103e17f74f16d05db1adf3
WhiteFrostune <3566621734@qq.com>
2026-04-01 22:02:00 +0800

    添加多态相关示例：虚函数、纯虚函数与抽象类、虚析构与纯虚析构

---
+++ Project_0031_纯虚函数和抽象类/Project_0031_纯虚函数和抽象类/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void eat() = 0;
+};
+
+class Cat {
+public:
+	virtual void eat() {
+		cout << "猫在吃东西" << endl;
+	}
+};
+int main() {
+	Cat c;
+	c.eat();
+
+	return 0;
+}
