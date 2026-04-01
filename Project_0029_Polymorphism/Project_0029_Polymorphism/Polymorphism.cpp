4f28261c22df9939d1c1a92a564c5dcbd0eed525
WhiteFrostune <3566621734@qq.com>
2026-04-01 12:50:56 +0800

    添加多态示例代码

---
+++ Project_0029_Polymorphism/Project_0029_Polymorphism/Polymorphism.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void eat() {
+		cout << "动物在吃东西" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void eat() {
+		cout << "猫在吃东西" << endl;
+	}
+};
+
+class Pig : public Animal {
+public:
+	void eat() {
+		cout << "猪在吃东西" << endl;
+	}
+};
+
+void eat(Animal& a) {
+	a.eat();
+}
+
+void test() {
+	Cat c;
+	Pig p;
+	eat(c);
+	eat(p);
+}
+
+int main() {
+	test();
+	return 0;
+}
